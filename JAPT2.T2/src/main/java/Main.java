43220e462dedf65e730390bb41109ab663022dfc
Dima <bodryi2001@icloud.com>
2024-05-25 18:26:50 +0400

    First commit

---
+++ JAPT2.T2/src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("11", "11"));
+        System.out.println(bins.mult("11", "11"));
+    }
+}
